7503b085ad63bfeabf5d883cbba3164f537140f8
Ayush <ayushgadag09@gmail.com>
2026-03-10 22:36:26 +0530

    Implemented field locating and input automation using Selenium

--- selenium_practice/test_login.py
+++ selenium_practice/test_login.py
@@ -7,8 +7,12 @@ driver = webdriver.Chrome()
 driver.get("https://www.saucedemo.com/")
 driver.maximize_window()
 
+username = driver.find_element(By.ID, "user-name")
 
+username.send_keys("gunesh")
 
+pass_word = driver.find_element(By.ID,"password")
+pass_word.send_keys("123")
 
 
 time.sleep(5)
